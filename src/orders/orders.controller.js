747609000169dffe67263ec5a97b5fc53a3387cd
Marcos A Rosas <contactme@marcosarosas.dev>
2023-03-11 13:08:31 -0600

    setup order route and list function

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -6,4 +6,28 @@ const orders = require(path.resolve("src/data/orders-data"));
 // Use this function to assigh ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /orders handlers needed to make the tests pass
+// validation functions
+function isMethodTypeIncluded(type){
+    return (req, res, next) => {
+        const {data = {}} = req.body;
+
+        if (data[type]){
+            return next();
+        } else {
+            next({
+                status: 400,
+                message: `Order must include a ${type}`,
+            });
+        }
+    };
+};
+
+// route main functions
+
+function list(req, res){
+    res.status(200).send({data: orders});
+};
+
+module.exports = {
+    list,
+};
--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,5 +1,11 @@
 const router = require("express").Router();
+const methodNotAllowed = require('../errors/methodNotAllowed');
+const controller = require('./orders.controller');
 
 // TODO: Implement the /orders routes needed to make the tests pass
+
+router.route('/')
+    .get(controller.list)
+    .all(methodNotAllowed);
 
 module.exports = router;
